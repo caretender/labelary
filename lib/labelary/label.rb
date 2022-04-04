1d128725d7ce53963a408ff384698de12ccb537f
Feliks Gurevich <feliks@caretender.co>
2022-04-04 10:48:51 -0400

    bumped faraday version

--- lib/labelary/label.rb
+++ lib/labelary/label.rb
@@ -1,7 +1,7 @@
 module Labelary
   class Label
-    def self.render(*args)
-      new(*args).render
+    def self.render(**args)
+      new(**args).render
     end
 
     def initialize(zpl, args = {})
